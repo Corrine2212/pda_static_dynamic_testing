c4483aba6a52c9d5e3d48e98a78f00544367f7d0
Corrine <corrine.sing@gmail.com>
2023-06-08 22:07:55 +0100

    first commit

---
+++ specs/card_game_tests.py
@@ -0,0 +1,24 @@
+import unittest
+from src.card import Card
+from src.card_game import CardGame
+
+class TestCardGame(unittest.TestCase):
+    def setUp(self):
+        self.game = CardGame()
+        self.card1 = Card("Spades", 1)
+        self.card2 = Card("Hearts", 5)
+        self.card3 = Card("Clubs", 7)
+        self.card4 = Card("Diamonds", 4)
+
+    def test_check_for_ace(self):
+        self.assertTrue(self.game.check_for_ace(self.card1))
+        self.assertFalse(self.game.check_for_ace(self.card2))
+
+    def test_highest_card(self):
+        highest = self.game.highest_card(self.card3, self.card4)
+        self.assertEqual(highest, self.card3)
+
+    def test_cards_total(self):
+        cards = [self.card1, self.card2, self.card3, self.card4]
+        total = self.game.cards_total(cards)
+        self.assertEqual(total, "You have a total of 17")
